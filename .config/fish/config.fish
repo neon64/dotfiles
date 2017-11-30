c6d60480306a888b7b713776c06e358e0da9ddc7
Chris Chamberlain <c.chamberlain@iinet.net.au>
2017-11-30 18:02:10 +1100

    Made fish u-vars syncable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -53,6 +53,35 @@ alias docker-compose "sudo docker-compose"
 alias vim "nvim"
 
 ### ========================================
+###				   COLOURS
+### ========================================
+
+# I'm not sure where these are from, but I'll put them here instead of
+# in the machine-specific u-vars file, so that they sync nicely.
+
+set fish_color_autosuggestion 555\x1eyellow
+set fish_color_command green
+set fish_color_comment red
+set fish_color_cwd blue
+set fish_color_cwd_root red
+set fish_color_end brmagenta
+set fish_color_error red\x1e\x2d\x2dbold
+set fish_color_escape cyan
+set fish_color_history_current cyan
+set fish_color_host normal
+set fish_color_match cyan
+set fish_color_normal white
+set fish_color_operator cyan
+set fish_color_param 00afff\x1ecyan
+set fish_color_quote brown
+set fish_color_redirection normal
+set fish_color_search_match \x2d\x2dbackground\x3dpurple
+set fish_color_selection \x2d\x2dbackground\x3dpurple
+set fish_color_user brgreen
+set fish_color_valid_path \x2d\x2dunderline
+set fisher_active_prompt clearance
+
+### ========================================
 ###				   AUTOSTART
 ### ========================================
 
