7887b0f09fe841c2362cae186e93f1ff35582b87
Chris Chamberlain <c.chamberlain@iinet.net.au>
2017-12-13 18:28:44 +1100

    Added suggestion to push

--- .config/fish/functions/check_dotf.fish
+++ .config/fish/functions/check_dotf.fish
@@ -1,19 +1,19 @@
-# function read_confirm
-#   while true
-#     read -l -p read_confirm_prompt confirm
+function read_confirm
+  while true
+    read -l -p read_confirm_prompt confirm
 
-#     switch $confirm
-#       case Y y
-#         return 0
-#       case '' N n
-#         return 1
-#     end
-#   end
-# end
+    switch $confirm
+      case ' ' Y y
+        return 0
+      case N n
+        return 1
+    end
+  end
+end
 
-# function read_confirm_prompt
-#   echo 'Are you sure you want to exit? [y/N] '
-# end
+function read_confirm_prompt
+  echo 'Would you like to push your local commits? [Y/n] '
+end
 
 function check_dotf 
     # strangely, this appears more robust
@@ -21,6 +21,11 @@ function check_dotf
     # sometimes it would return an error code but
     # there were no changed files 
     if dotf status | grep --quiet "nothing to commit"
+        if dotf status | grep --quiet "Your branch is ahead of 'origin/master'"
+            if read_confirm
+                dotf push
+            end
+        end
         return 0
     else
         echo ""
