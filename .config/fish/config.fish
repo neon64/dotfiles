262b57ef4f9064e0a46916e974d4db6389dae620
Chris Chamberlain <c.chamberlain@iinet.net.au>
2018-01-04 13:33:23 +1100

    Disowned browser process from `xdg-open` inside `g` shell function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -62,8 +62,9 @@ alias docker-compose "sudo docker-compose"
 # use neovim
 alias vim "nvim"
 function g
-	set query $argv
-	xdg-open "https://www.google.com/search?q=$query"
+    set query $argv
+    xdg-open "https://www.google.com/search?q=$query"&
+    disown
     # try one or the other
     i3-msg workspace 1 2>/dev/null 1>/dev/null
     swaymsg workspace "1: " > /dev/null
