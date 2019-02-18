344ce2ceafa8164263f08e33dbbc09ac5bca311d
Chris Chamberlain <c.chamberlain@iinet.net.au>
2019-02-19 09:49:38 +1100

    Added newline at end of ytdl.fish

--- .config/fish/functions/ytdl.fish
+++ .config/fish/functions/ytdl.fish
@@ -1,3 +1,3 @@
 function ytdl
-    youtube-dl -o "~/Music/YouTube/%(title)s.%(ext)s" --extract-audio --metadata-from-title "%(artist)s - %(title)s" --add-metadata --audio-format mp3 "$argv"
+    youtube-dl -o "~/Music/YouTube/%(title)s.%(ext)s" --extract-audio --metadata-from-title "%(artist)s - %(title)s" --add-metadata --audio-format m4a "$argv"
 end
