c8f52909a391900c9ebfce02e781d724916cc969
Chris Chamberlain <c.chamberlain@iinet.net.au>
2019-07-06 14:16:10 +1000

    Improved `check_dotf` script

--- .config/fish/functions/check_dotf.fish
+++ .config/fish/functions/check_dotf.fish
@@ -1,8 +1,8 @@
 function read_confirm
   while true
-    read -l -p read_confirm_prompt confirm
+    read -l confirm -p "$argv[1]"
 
-    switch $confirm
+    switch "$confirm"
       case ' ' Y y
         return 0
       case N n
@@ -11,58 +11,77 @@ function read_confirm
   end
 end
 
-function read_confirm_prompt
-  echo 'Would you like to push your local commits? [Y/n] '
-end
-
-function is_pending_push
-    set --local git_unpushed_commits
-    set --local git_unpulled_commits
-    set --local unpulled_commits ⇣
-    set --local unpushed_commits ⇡
-
+function get_unpushed_commits
     set --local has_upstream (command dotf rev-parse --abbrev-ref '@{upstream}' 2>/dev/null)
     if test -n "$has_upstream"  # check there is an upstream repo configured
         and test "$has_upstream" != '@{upstream}' # Fixed #179, dont check the empty repo
         command dotf rev-list --left-right --count 'HEAD...@{upstream}' \
         | read --local --array git_status
         set --local commit_to_push $git_status[1]
-        set --local commit_to_pull $git_status[2]
-
-        if test $commit_to_push -gt 0  # upstream is behind local repo
-            set git_unpushed_commits "$unpushed_commits"
-        end
-
-        if test $commit_to_pull -gt 0  # upstream is ahead of local repo
-            set git_unpulled_commits "$unpulled_commits"
-        end
+        echo $commit_to_push
     end
-
-    echo "$git_unpushed_commits$git_unpulled_commits"
 end
+
+function get_unpulled_commits
+    set --local has_upstream (command dotf rev-parse --abbrev-ref '@{upstream}' 2>/dev/null)
+    if test -n "$has_upstream"  # check there is an upstream repo configured
+        and test "$has_upstream" != '@{upstream}' # Fixed #179, dont check the empty repo
+        command dotf rev-list --left-right --count 'HEAD...@{upstream}' \
+        | read --local --array git_status
+        set --local commit_to_pull $git_status[2]
+        echo $commit_to_pull
+    end
+end
+
+function commit_prompt
+    echo -e (set_color green)'Would you like to commit them?'(set_color normal)' [Y/n] '
+end
+
+function push_prompt
+    echo -e (set_color green)'Would you like to push them?'(set_color normal)' [Y/n] '
+end
+
+function pull_prompt
+    echo -e (set_color green)'Would you like to pull them?'(set_color normal)' [Y/n] '
+end
+
 
 function check_dotf
     # strangely, this appears more robust
     # than `dotf diff-index --quiet HEAD`.
     # sometimes it would return an error code but
     # there were no changed files
-    if dotf status | grep --quiet "nothing to commit"
+    if dotf status | grep --quiet -v "nothing to commit"
         echo ""
-    else
-        echo ""
-        echo (set_color red)"You have unsaved changes to your dotfiles:"(set_color normal)
+        echo "You have unsaved changes to your dotfiles:"
         echo ""
         dotf -C $HOME status -s
         echo ""
-        echo "You should probably commit them sometime soon."
+        if read_confirm commit_prompt
+            dotf commit -a
+        end
+        echo ""
     end
 
-    echo "pending push"
-    is_pending_push
+    if [ "$argv[1]" = "--fetch" ]
+        dotf fetch
+    end
 
-    if dotf status | grep --color=never "Your branch is ahead of 'origin/master'"
-        if read_confirm
+    set unpulled_commits (get_unpulled_commits)
+    set unpushed_commits (get_unpushed_commits)
+
+    if test $unpulled_commits -gt 0  # upstream is behind local repo
+        echo (set_color cyan)"There are new updates to the dotfiles."(set_color normal)
+        if read_confirm pull_prompt
+            dotf pull
+        end
+    end
+
+    if test $unpushed_commits -gt 0
+        echo (set_color cyan)"You have unpushed local commits."(set_color normal)
+        if read_confirm push_prompt
             dotf push
         end
     end
+
 end
