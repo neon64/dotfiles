d97972e0b73733e31882d10637ac74bef970226f
Chris Chamberlain <chris@chamberlain.id.au>
2020-08-17 15:23:29 +1000

    Tried to autounlock gnome keyring, moved session launcher into dotfiles
    The session launcher is called `fdm`, standing for 'fzf-display
    manager'. It is really simple - basically just a bash script which
    contains presets for Xorg and Wayland sessions which I like to use.
    
    Also added multi-monitor support to sway config, with Mod+n and Mod+m to
    move a workspace between monitors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -54,13 +54,15 @@ set -x FZF_CTRL_T_OPTS "--preview 'highlight --force --out-format=ansi {} | head
 if status --is-login && status --is-interactive
     if set -q XDG_VTNR && test "$XDG_VTNR" -eq "1" && test ! -e /tmp/sway-autoopen.tag
         touch /tmp/sway-autoopen.tag
-        env _JAVA_AWT_WM_NONREPARENTING=1 MOZ_ENABLE_WAYLAND=1 QT_QPA_PLATFORM=wayland-egl QT_STYLE_OVERRIDE=adwaita XDG_SESSION_TYPE=wayland systemd-cat -t sway sway --my-next-gpu-wont-be-nvidia
+
+        # https://wiki.archlinux.org/index.php/GNOME/Keyring#xinitrc_method
+        bash -c 'eval $(/usr/bin/gnome-keyring-daemon --start); export SSH_AUTH_SOCK; _JAVA_AWT_WM_NONREPARENTING=1 MOZ_ENABLE_WAYLAND=1 QT_QPA_PLATFORM=wayland-egl QT_STYLE_OVERRIDE=adwaita XDG_SESSION_TYPE=wayland systemd-cat -t sway sway --my-next-gpu-wont-be-nvidia'
     end
     if set -q XDG_VTNR && test "$XDG_VTNR" -gt "0" -a "$XDG_VTNR" -lt "6"
         # set LINE_UP "\033[1A"
         set CLEAR_LINE "\033[K"
         echo -e "$LINE_UP$CLEAR_LINE$LINE_UP$CLEAR_LINE$LINE_UP$CLEAR_LINE$LINE_UP"
-        exec ~/code/fdm/fdm
+        exec ~/.config/bin/fdm
     end
 end
 
